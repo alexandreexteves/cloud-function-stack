02347d7f04f118669a23713723e35afb6b7a22a7
Alexandre <alexandreexteves@gmail.com>
2024-05-13 09:47:46 -0300

    correcao

--- cf_sms/main.py
+++ cf_sms/main.py
@@ -1,4 +1,5 @@
 import base64
 
-def hello_pubsub(cloud_event):
-    print(base64.b64decode(cloud_event.data["message"]["data"]))
+def hello_pubsub(event, context):
+    pubsub_message = base64.b64decode(event['data']).decode('utf-8')
+    print(pubsub_message)
